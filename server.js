5f40909ee2fe0bd8603092292f29fcf0cc47cf4a
marloestacx <marloes.tacx@live.nl>
2022-06-01 13:29:13 +0200

    Update: code from index to server

--- server.js
+++ server.js
@@ -1,64 +1,66 @@
-const express = require("express");
+import path from "path";
+import express from "express";
+import { fileURLToPath } from "url";
+import * as prismicH from "@prismicio/helpers";
+import { client } from "./config/prismicConfig.js";
+
 const app = express();
 const port = process.env.PORT || 3000;
-const fetch = (...args) =>
-  import("node-fetch").then(({ default: fetch }) => fetch(...args));
-
-app.set("view engine", "ejs");
-app.set("views", "./views");
-app.use(express.static("public"));
 
 const options = {
-  weekday: 'long'
-}
+  weekday: "long",
+};
 
 let today = new Date().toLocaleDateString("nl-NL", options);
 let available = [];
 let unavailable = [];
 
+// Set EJS as templating engine
+app.set("view engine", "ejs");
+app.set("views", "./views");
+app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  fetch(
-    "https://raw.githubusercontent.com/ROEL2407/cmda-smoelenboek/main/docent.json"
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      available = [];
-      unavailable = [];
-      data.docenten.forEach(docent => {
-        
-        if (docent.dagen_a.includes(today)) {
-          available.push({
-            docent: docent
-          })
-        }
-        else {
-          unavailable.push({
-            docent: docent
-          })
-        }
+// Add a middleware function that runs on every route. It will inject
+// the prismic context to the locals so that we can access these in
+// our templates.
+app.use((req, res, next) => {
+  res.locals.ctx = {
+    prismicH,
+  };
+  next();
+});
+
+// Query for the root path.
+app.get("/", async (req, res) => {
+  // Here we are retrieving the first document from your API endpoint
+  const document = await client.getAllByType("persoon");
+  available = [];
+  unavailable = [];
+  document.forEach((docent) => {
+    if (docent.data.dagen_aanwezig[0].text.toLowerCase().includes(today)) {
+      available.push({
+        docent: docent,
       });
-      console.log(available);
-      res.render("index", { today: available, not_today: unavailable });
-    });
+    } else {
+      unavailable.push({
+        docent: docent,
+      });
+    }
+  });
+  res.render("index", { today: available, not_today: unavailable });
 });
 
-// get specific info of 1 teacher and display on page
-app.get("/detail/:id", function (req, res) {
+app.get("/detail/:id", async (req, res) => {
   console.log(req.params.id);
-  fetch(
-    `https://www.rijksmuseum.nl/api/nl/collection/${req.params.id}`
-  )
-    .then({
-    })
-    .catch((err) => res.send(err));
+  const document = await client.getByID(req.params.id);
+  res.render("detail", { document });
 });
-
 
 app.get("/create", (req, res) => {
-      res.render("create", { });
+  res.render("create", {});
 });
 
+// Listen to application port.
 app.listen(port, () => {
-  console.log("listening on port ", port);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
