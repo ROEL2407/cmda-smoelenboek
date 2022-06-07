7ad047046b4155c8bbc2c17e69b593f6f5be580a
marloestacx <marloes.tacx@live.nl>
2022-06-07 13:38:09 +0200

    Update: sort on last name

--- routes/homeRoute.js
+++ routes/homeRoute.js
@@ -3,48 +3,20 @@ var app = express();
 export const homeRoute = express.Router();
 import { client } from "../config/prismicConfig.js";
 
-const options = {
-  weekday: "long",
-};
-
-let today = new Date().toLocaleDateString("nl-NL", options);
-let available = [];
-let unavailable = [];
-let categories = [];
-
 homeRoute.get("/", async (req, res) => {
   // Here we are retrieving the first document from your API endpoint
   const document = await client.getAllByType("persoon");
-  document.forEach((docent) => {
-    if (!categories.includes(docent.data.specaliteit)) {
-      //  only runs if value not in array
-      categories.push(docent.data.specaliteit);
-    }
-  });
+
+  // sort on last name
+  document.sort((a, b) =>
+    a.data.achternaam[0].text > b.data.achternaam[0].text
+      ? 1
+      : b.data.achternaam[0].text > a.data.achternaam[0].text
+      ? -1
+      : 0
+  );
 
   res.render("index", {
     docenten: document,
-    categories,
   });
 });
-
-// homeRoute.get("/:id", async (req, res) => {
-//   console.log(req.params.id);
-//   const document = await client.getByID(req.params.id);
-//   // console.log(document.data.specaliteit);
-//   const teachers = await client.getAllByType("persoon");
-//   let relatedTeachers = [];
-//   teachers.forEach((docent) => {
-//     if (docent.data.naam[0].text !== document.data.naam[0].text) {
-//       console.log(docent.data.naam[0].text);
-//       console.log(document.data.naam[0].text);
-//       if (document.data.specaliteit == docent.data.specaliteit) {
-//         relatedTeachers.push({
-//           docent: docent,
-//         });
-//       }
-//     }
-//   });
-
-//   res.render("detail", { document, categories, teachers: relatedTeachers });
-// });
