0e26b476d6894b0ae50bb97d6a2d6ed8a7ac2bec
marloestacx <marloes.tacx@live.nl>
2022-06-07 13:46:11 +0200

    Update: deleted unnecessary code

--- routes/detailRoute.js
+++ routes/detailRoute.js
@@ -33,37 +33,13 @@ const options = {
   weekday: "long",
 };
 
-let today = new Date().toLocaleDateString("nl-NL", options);
-let available = [];
-let unavailable = [];
-let categories = [];
-
-// detailRoute.get("/", async (req, res) => {
-//   // Here we are retrieving the first document from your API endpoint
-//   const document = await client.getAllByType("persoon");
-//   document.forEach((docent) => {
-//     if (!categories.includes(docent.data.specaliteit)) {
-//       //  only runs if value not in array
-//       categories.push(docent.data.specaliteit);
-//     }
-//   });
-
-//   res.render("index", {
-//     docenten: document,
-//     categories,
-//   });
-// });
-
 detailRoute.get("/:id", async (req, res) => {
   console.log(req.params.id);
   const document = await client.getByID(req.params.id);
-  // console.log(document.data.specaliteit);
   const teachers = await client.getAllByType("persoon");
   let relatedTeachers = [];
   teachers.forEach((docent) => {
     if (docent.data.naam[0].text !== document.data.naam[0].text) {
-      console.log(docent.data.naam[0].text);
-      console.log(document.data.naam[0].text);
       if (document.data.specaliteit == docent.data.specaliteit) {
         relatedTeachers.push({
           docent: docent,
@@ -71,6 +47,5 @@ detailRoute.get("/:id", async (req, res) => {
       }
     }
   });
-
-  res.render("detail", { document, categories, teachers: relatedTeachers });
+  res.render("detail", { document, teachers: relatedTeachers });
 });
