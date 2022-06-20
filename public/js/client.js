2e9856eba33d0bc6db2e4864460204131f3b7255
ROEL2407 <roel24komen@gmail.com>
2022-06-20 11:40:01 +0200

    Update: idle timeout added

--- public/js/client.js
+++ public/js/client.js
@@ -122,3 +122,30 @@ function letterClicked(clicked_id) {
   oldId = clicked_id;
   link.classList.add("letterClicked");
 }
+
+// // timeout to go back to home
+function idleKick() {
+
+  const idleDurationSecs = 300;    // X number of seconds
+  const redirectUrl = '/';  // Redirect idle users to this URL
+  let idleTimeout; // variable to hold the timeout, do not modify
+
+  const resetIdleTimeout = function() {
+
+      // Clears the existing timeout
+      if(idleTimeout) clearTimeout(idleTimeout);
+
+      // Set a new idle timeout to load the redirectUrl after idleDurationSecs
+      idleTimeout = setTimeout(() => location.href = redirectUrl, idleDurationSecs * 1000);
+  };
+
+  // Init on page load
+  resetIdleTimeout();
+
+  // Reset the idle timeout on any of the events listed below
+  ['click', 'touchstart', 'mousemove'].forEach(evt => 
+      document.addEventListener(evt, resetIdleTimeout, false)
+  );
+
+}
+idleKick();
