8cc4a484afb090206ed2f113cb3e8050d1e0db10
marloestacx <marloes.tacx@live.nl>
2022-06-20 14:22:47 +0200

    Create: search on filter

---
+++ routes/searchFilterRoute.js
@@ -0,0 +1,53 @@
+import e from "express";
+import express from "express";
+export const searchFilterRoute = express.Router();
+import { client } from "../config/prismicConfig.js";
+
+searchFilterRoute.get("/", async (req, res) => {
+  const document = await client.getAllByType("persoon");
+  let docenten = [];
+
+  if (filterTeachers.length == 0) {
+    document.forEach((docent) => {
+      let firstName = docent.docent.data.naam[0].text;
+      let lastName = docent.docent.data.achternaam[0].text;
+      let fullName = firstName + " " + lastName;
+
+      if (fullName.toLowerCase().includes(req.query.q)) {
+        docenten.push({
+          docent: docent.docent,
+        });
+      }
+    });
+  } else {
+    //get all teachers with search text
+    filterTeachers.forEach((docent) => {
+      let firstName = docent.docent.data.naam[0].text;
+      let lastName = docent.docent.data.achternaam[0].text;
+      let fullName = firstName + " " + lastName;
+
+      if (fullName.toLowerCase().includes(req.query.q)) {
+        docenten.push({
+          docent: docent.docent,
+        });
+      }
+    });
+  }
+
+  let result;
+  let teachers = [];
+  if (docenten.length == 0) {
+    const random = document.sort(() => 0.5 - Math.random());
+    teachers = random.slice(0, 3);
+
+    result =
+      "<p> Geen zoekresultaten gevonden voor: " +
+      req.query.q +
+      "<p>" +
+      "<p> misschien zoek je een van deze docenten </p>";
+  } else {
+    let specaliteit = docenten[0].docent.data.specaliteit;
+    result = "<p> Resultaten voor " + req.query.q + " en " + specaliteit;
+  }
+  res.render("search", { docenten, result, teachers });
+});
